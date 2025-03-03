32c96be37281ffb75fd1cb1a76da4aa835b699ac
prashant9759 <prashantkumar.20255@gmail.com>
2025-03-03 15:06:23 +0530

    changed debug mode

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
         db.create_all()
         seed_cuisines_and_food_preferences()
         port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-        app.run(host="0.0.0.0", port=port)
+        app.run(host="0.0.0.0", port=5000, debug=False)
        
 
 
